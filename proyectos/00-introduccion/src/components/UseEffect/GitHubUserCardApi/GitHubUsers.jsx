3e56f208663f67121f69c54d12548b04507b65de
Ambrosio03 <ambrosio0304@gmail.com>
2024-01-16 14:45:22 +0100

    spiner

--- proyectos/00-introduccion/src/components/UseEffect/GitHubUserCardApi/GitHubUsers.jsx
+++ proyectos/00-introduccion/src/components/UseEffect/GitHubUserCardApi/GitHubUsers.jsx
@@ -30,16 +30,17 @@ useEffect(() => {
     <>
         <div className='mx-auto max-w-[75%] text-center relative'>
             <h1 className='text-4x1 font-bold mb-8 mt-2'>Usando UseEffect para realizar un fetch de la api gitHUb</h1>
-            <Spinner></Spinner>
-            <div className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mx-auto relative'>
-                {users.map(user => (
-                    <CardGitHub 
-                    key = {user.id}
-                    avatar_url = {user.avatar_url}
-                    login ={user.login}
-                    html_url={user.html_url}></CardGitHub>
-                ))}
-            </div>
+            {loading ? (<Spinner/>) : (
+                <div className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mx-auto relative'>
+                    {users.map(user => (
+                        <CardGitHub 
+                        key = {user.id}
+                        avatar_url = {user.avatar_url}
+                        login ={user.login}
+                        html_url={user.html_url}></CardGitHub>
+                    ))}
+                </div>
+            )}
         </div>
     </>
   )
